771c7abc18e4e75429d4008ddbc89b835b5e2ae9
matthijsvandermaas <matthijsvandermaas@yahoo.com>
2023-09-28 12:19:58 +0200

    first commit

---
+++ src/main/java/com/eindopdracht/eindopdracht_fs_matthijs_van_der_maas/EindopdrachtFsMatthijsVanDerMaasApplication.java
@@ -0,0 +1,13 @@
+package com.eindopdracht.eindopdracht_fs_matthijs_van_der_maas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EindopdrachtFsMatthijsVanDerMaasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EindopdrachtFsMatthijsVanDerMaasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eindopdracht/eindopdracht_fs_matthijs_van_der_maas/EindopdrachtFsMatthijsVanDerMaasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eindopdracht.eindopdracht_fs_matthijs_van_der_maas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EindopdrachtFsMatthijsVanDerMaasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
